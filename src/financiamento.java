d41b791203ad51fa082e40fa511130abcd6dfab3
Natália Freitas <natyff1@hotmail.com>
2020-05-27 20:04:42 -0300

    Leitura do financiamento

--- src/financiamento.java
+++ src/financiamento.java
@@ -1,9 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class financiamento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		double salario,PorcentagemE, ValorF,prestacoes;
+		int opcao;
+		
+	//LEITURA DO NOME E SALARIO
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.print("Nome do cliente: ");
+		nome=sc.nextLine();
+		System.out.println("Salario: ");
+		salario=sc.nextDouble();
+		
+		while(salario < 0.0) {
+			System.out.println("VALOR INVALIDO, POR FAVOR DIGITE NOVAMENTE!!");
+			salario=sc.nextDouble();
+		}
+		
+	
+		sc.close();
 	}
 
 }
