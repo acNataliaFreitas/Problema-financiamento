b5f5ee68a7c5e989d9b51f9f0f62a0981603f4cf
Natália Freitas <natyff1@hotmail.com>
2020-05-27 20:20:01 -0300

    Processamento financiado

--- src/financiamento.java
+++ src/financiamento.java
@@ -24,6 +24,50 @@ public class financiamento {
 			salario=sc.nextDouble();
 		}
 		
+		//LEITURA DO FINANCIAMENTO
+				System.out.println("Numero de Pestaçoes: ");
+				prestacoes=sc.nextDouble();
+				System.out.println("Porcentagem de entrada: ");
+				PorcentagemE=sc.nextDouble();
+				System.out.println("Valor total do financiamente: ");
+				ValorF=sc.nextDouble();
+				
+			double prestacao = ValorF *(100.0 - PorcentagemE)/100.0/prestacoes;
+			
+			while(prestacoes > 0.3 * salario) {
+				System.out.print("Capacidade de pagamento excedida! Tente novamente: ");
+				ValorF=sc.nextDouble();
+				prestacao = ValorF *(100.0 - PorcentagemE)/100.0/prestacoes;
+			}
+				
+			//PROCESSAMENTO FINANCIADO
+			double entrada = ValorF * PorcentagemE /100.0;
+			
+			do {
+				System.out.println("MENU:");
+				System.out.println("1 - Mostrar valor da entrada");
+				System.out.println("2 - Mostrar o valor financiado");
+				System.out.println("3 - Mostrar valor de cada prestação");
+				System.out.println("4 - Sair");
+				System.out.println("Digite uma opcao: ");
+				opcao=sc.nextInt();
+				
+				if(opcao==1) {
+					System.out.printf("ENTRADA = R$ %.2f%n", entrada);
+				}
+				else if(opcao==2) {
+					System.out.printf("VALOR FINANCIADO = R$%.2f%n", ValorF);
+				}
+				else if(opcao==3) {
+					System.out.printf("VALOR DE CADA PRESTAÇÃO = R$%.2f%n", prestacao);
+				}
+				else if(opcao!=4) {
+					System.out.println("OPÇÃO INVALIDA");
+				}
+				
+				
+			}while(opcao!=4) ; 
+			System.out.println("FIM DO PROGRAMA!!");	
 	
 		sc.close();
 	}
